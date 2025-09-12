fb326c2a7572fa58f8334e62004e16c8a970a288
danidesouzalimalg-sketch <dani.desouzalimalg@gmail.com>
2025-09-11 21:25:44 -0300

    Add files via upload

---
+++ exer48.php
@@ -0,0 +1,18 @@
+<?php
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2A
+Componentes:
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Pedro Bueno (RA: 25181992-2)        
+Data: 28 de Agosto de 2025
+Descritivo: Escreva um programa que calcule a soma de dois números
+*******************************************************************************/
+$temp = 20;
+    echo ($temp < 15) ? "Esta frio" : (($temp <= 25) ? "Esta agradável" : "Esta quente");
+?>
