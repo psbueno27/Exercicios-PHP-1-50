1a95705c1e08191f04113c9fd329cd55fc2370e2
Juan Siqueira <juanpablopss.12@gmail.com>
2025-09-14 18:19:30 -0300

    Update exercicio12.php

--- exercicio12.php
+++ exercicio12.php
@@ -1,18 +1,18 @@
 <?php
 /******************************************************************************
-Curso: Engenharia de Software
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ESOFT-2A
-Componentes:
-                Alef Luciano (RA: 25004652-2)
-                Daniel de Souza (RA: 25143755-2)
-                João Pedro (RA: 25168486-2)
-                Juan Pablo (RA: 25181903-2)
-                Pedro Bueno (RA: 25181992-2)
-Data: 10 de Setembro de 2025
-Descritivo: Crie um programa que use += para adicionar 10 a uma variável.
-*******************************************************************************/
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                    Alef Luciano (RA: 25004652-2)
+                    Daniel de Souza (RA: 25143755-2)
+                    João Pedro (RA: 25168486-2)
+                    Juan Pablo (RA: 25181903-2)
+                    Pedro Bueno (RA: 25181992-2)
+                    Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
+
+    *******************************************************************************/
 
 $numb = 18;
 $numb += 10;
@@ -20,3 +20,4 @@ $numb += 10;
 echo "O valor atual é $numb.";
 
 ?>
+
