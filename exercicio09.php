a07f1c6c72391746174d50e4206754f5bbc144a3
Juan Siqueira <juanpablopss.12@gmail.com>
2025-09-14 18:18:47 -0300

    Update exercicio09.php

--- exercicio09.php
+++ exercicio09.php
@@ -1,19 +1,19 @@
 <?php
 
 /******************************************************************************
-Curso: Engenharia de Software
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ESOFT-2A
-Componentes:
-                Alef Luciano (RA: 25004652-2)
-                Daniel de Souza (RA: 25143755-2)
-                João Pedro (RA: 25168486-2)
-                Juan Pablo (RA: 25181903-2)
-                Pedro Bueno (RA: 25181992-2)
-Data: 28 de Agosto de 2025
-Descritivo: Faça um programa que calcule o aumento de 15% sobre um valor.
-*******************************************************************************/
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                    Alef Luciano (RA: 25004652-2)
+                    Daniel de Souza (RA: 25143755-2)
+                    João Pedro (RA: 25168486-2)
+                    Juan Pablo (RA: 25181903-2)
+                    Pedro Bueno (RA: 25181992-2)
+                    Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
+
+    *******************************************************************************/
 
 // Declaração de variáveis
 $num = 1500;
