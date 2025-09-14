b2b423dcf1c52ae4fdae99d132f92a2b6f7e61c8
Juan Siqueira <juanpablopss.12@gmail.com>
2025-09-14 18:20:09 -0300

    Update exercicio15.php

--- exercicio15.php
+++ exercicio15.php
@@ -1,22 +1,22 @@
 <?php
 /******************************************************************************
-Curso: Engenharia de Software
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ESOFT-2A
-Componentes:
-                Alef Luciano (RA: 25004652-2)
-                Daniel de Souza (RA: 25143755-2)
-                João Pedro (RA: 25168486-2)
-                Juan Pablo (RA: 25181903-2)
-                Pedro Bueno (RA: 25181992-2)
-Data: 10 de Setembro de 2025
-Descritivo: Crie um programa que use %= para armazenar o resto de uma divisão.
-*******************************************************************************/
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                    Alef Luciano (RA: 25004652-2)
+                    Daniel de Souza (RA: 25143755-2)
+                    João Pedro (RA: 25168486-2)
+                    Juan Pablo (RA: 25181903-2)
+                    Pedro Bueno (RA: 25181992-2)
+                    Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
 
+    *******************************************************************************/
 $numb = 360;
 $numb %= 7;
 
 echo "O resto é $numb.";
+
 
 ?>
