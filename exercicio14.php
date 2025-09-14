fbb835160265e9f14a28261feb927c3261649c54
Juan Siqueira <juanpablopss.12@gmail.com>
2025-09-14 18:19:49 -0300

    Update exercicio14.php

--- exercicio14.php
+++ exercicio14.php
@@ -1,18 +1,18 @@
 <?php
 /******************************************************************************
-Curso: Engenharia de Software
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ESOFT-2A
-Componentes:
-                Alef Luciano (RA: 25004652-2)
-                Daniel de Souza (RA: 25143755-2)
-                João Pedro (RA: 25168486-2)
-                Juan Pablo (RA: 25181903-2)
-                Pedro Bueno (RA: 25181992-2)
-Data: 10 de Setembro de 2025
-Descritivo: Escreva um código que use /= para reduzir um valor pela metade.
-*******************************************************************************/
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                    Alef Luciano (RA: 25004652-2)
+                    Daniel de Souza (RA: 25143755-2)
+                    João Pedro (RA: 25168486-2)
+                    Juan Pablo (RA: 25181903-2)
+                    Pedro Bueno (RA: 25181992-2)
+                    Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
+
+    *******************************************************************************/
 
 $numb = 30;
 $numb /= 2;
@@ -20,3 +20,4 @@ $numb /= 2;
 echo "O valor atual é $numb.";
 
 ?>
+
