4c068e9f1291a35b9d1f36301337fcf848e36c1f
Juan Siqueira <juanpablopss.12@gmail.com>
2025-09-14 18:19:40 -0300

    Update exercicio13.php

--- exercicio13.php
+++ exercicio13.php
@@ -1,22 +1,23 @@
 <?php
 /******************************************************************************
-Curso: Engenharia de Software
-Disciplina: Linguagem e Técnicas de Programacão
-Professor: Flores
-Turma: ESOFT-2A
-Componentes:
-                Alef Luciano (RA: 25004652-2)
-                Daniel de Souza (RA: 25143755-2)
-                João Pedro (RA: 25168486-2)
-                Juan Pablo (RA: 25181903-2)
-                Pedro Bueno (RA: 25181992-2)
-Data: 10 de Setembro de 2025
-Descritivo: Faça um script que use *= para dobrar o valor de uma variável.
-*******************************************************************************/
+    Curso: Engenharia de Software
+    Disciplina: Linguagem e Técnicas de Programacão
+    Professor: Flores
+    Turma: ESOFT-2A
+    Componentes:
+                    Alef Luciano (RA: 25004652-2)
+                    Daniel de Souza (RA: 25143755-2)
+                    João Pedro (RA: 25168486-2)
+                    Juan Pablo (RA: 25181903-2)
+                    Pedro Bueno (RA: 25181992-2)
+                    Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
+
+    *******************************************************************************/
 
 $numb = 124;
 $numb *= 2;
 
 echo "O valor atual é $numb.";
+
 
 ?>
