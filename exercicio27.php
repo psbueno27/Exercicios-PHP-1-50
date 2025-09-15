c46a599ced9eb0972b023efe1eb651542ca99796
Juan Siqueira <juanpablopss.12@gmail.com>
2025-09-14 22:22:09 -0300

    Add files via upload

--- exercicio27.php
+++ exercicio27.php
@@ -5,10 +5,12 @@
     Professor: Flores
     Turma: ESOFT-2A
     Componentes:
-                    João Pedro (RA: 25168486-2)
-                    Juan Pablo (RA: 25181903-2)
-                    Pedro Bueno (RA: 25181992-2)
-                    Carlos Eduardo Galdino Sousa (RA: 25148436-2)
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Pedro Bueno (RA: 25181992-2)
+                Carlos Eduardo Galdino Sousa (RA: 25148436-2)
     Data: 14 de Setembro de 2025
     Descritivo: Teste se uma variável é maior que 100 OU menor que 50
     *******************************************************************************/
