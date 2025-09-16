c9aca364fd92e1db345b385054e4d6b433b66f02
psbueno27 <buenopedro07@gmail.com>
2025-09-16 16:57:53 -0300

    Update and rename ativade32.php to exercicio32.php

--- exercicio32.php
+++ exercicio32.php
@@ -5,7 +5,13 @@ Disciplina: Linguagem e Técnicas de Programação
 Professor: Flores
 Turma: ESOFT-2A
 Componentes:
-    RA - Nome do Aluno
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Pedro Bueno (RA: 25181992-2)
+                Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
+    
 Data: 16 de Setembro de 2025
 Descritivo: Classifique notas (A, B, C, D, F) e divida em Aprovado ou Reprovado, com entrada de usuário
 **************************/
@@ -92,3 +98,4 @@ if (isset($_POST['nota'])) {
 
 </body>
 </html>
+
