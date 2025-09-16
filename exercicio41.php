283b9f6c52eb0681d7a312637b1e27c3d75f5f83
psbueno27 <buenopedro07@gmail.com>
2025-09-16 16:59:16 -0300

    Update and rename ativade41.php to exercicio41.php

--- exercicio41.php
+++ exercicio41.php
@@ -5,7 +5,13 @@ Disciplina: Linguagem e Técnicas de Programação
 Professor: Flores
 Turma: ESOFT-2A
 Componentes:
-    RA - Nome do Aluno
+                Alef Luciano (RA: 25004652-2)
+                Daniel de Souza (RA: 25143755-2)
+                João Pedro (RA: 25168486-2)
+                Juan Pablo (RA: 25181903-2)
+                Pedro Bueno (RA: 25181992-2)
+                Carlos Eduardo Galdino Sousa (RA: 25148436-2)        
+    
 Data: 16 de Setembro de 2025
 Descritivo: Use o operador ternário para verificar se um número é positivo ou negativo
 **************************/
@@ -82,3 +88,4 @@ if (isset($_POST['numero'])) {
 
 </body>
 </html>
+
